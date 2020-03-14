cc7cbdeccd2646deb780318a2e928b945415960a
SamarkinaAV <opal-nii@yandex.ru>
2020-03-14 22:05:45 +0400

    feat: add shift .portfolio__image in portfolio__block

--- script.js
+++ script.js
@@ -1,15 +1,41 @@
+window.onload = function () {
+    addNavClickHandler();
+    addBorderInPortfolioImg();
+    addTagsClickHandler();
+};
+
+const PORTFOLIO = document.querySelector('.portfolio__block');
+const MENU = document.querySelector('.navigation');
+const TAGS = document.querySelector('.portfolio__tags');
+
+
 // selected item menu in navigation
-document.querySelector('.navigation').addEventListener('click', (event) => {
-    document.querySelector('.navigation').querySelectorAll('a').forEach(el => el.classList.remove('navigation__link-active'));
-    event.target.classList.add('navigation__link-active');
-});
+const addNavClickHandler = () => {
+    MENU.addEventListener('click', (event) => {
+        MENU.querySelectorAll('a').forEach(el => el.classList.remove('navigation__link-active'));
+        event.target.classList.add('navigation__link-active');
+    });
+};
 
 // add border around img in portfolio-block
-document.querySelector('.portfolio__block').addEventListener('click', (event) => {
-    if (event.target.classList.contains('portfolio__image')) {
-        document.querySelector('.portfolio__block').querySelectorAll('.portfolio__image').forEach(el => el.classList.remove('portfolio__image-active'));
-        event.target.classList.add('portfolio__image-active');
-    };
-});
+const addBorderInPortfolioImg = () => {
+    PORTFOLIO.addEventListener('click', (event) => {
+        if (event.target.classList.contains('portfolio__image')) {
+            PORTFOLIO.querySelectorAll('.portfolio__image').forEach(el => el.classList.remove('portfolio__image-active'));
+            event.target.classList.add('portfolio__image-active');
+        };
+    });
+};
 
-
+// selected tag in portfolio-block
+const addTagsClickHandler = () => {
+    TAGS.addEventListener('click', (event) => {
+        if (event.target.classList.contains('tag')) {
+            TAGS.querySelectorAll('.tag').forEach(el => el.classList.remove('tag-active'));
+            event.target.classList.add('tag-active');
+            // shift img clicked tag
+            let portfolioList = PORTFOLIO.querySelectorAll('.portfolio__image');
+            PORTFOLIO.prepend(portfolioList[portfolioList.length - 1]);
+        };
+    });
+};
